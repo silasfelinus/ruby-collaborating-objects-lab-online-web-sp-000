1942a7d7e04c9716ff72e6126e5a9dc3fbe9888f
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-02 22:16:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,18 @@ class Song
   end
 
   def self.new_by_filename(filename)
+    song_name = filename[/-/].last
+    artist = filename[/-/]
+    new_song = Song.new(song_name)
+    new_song.artist = artist
+    new_song
+  end
+
+  def artist_name=(name)
+    Artist.find_or_create_by_name(name).add_song(self)
+  end
+
+
 
 
 end
