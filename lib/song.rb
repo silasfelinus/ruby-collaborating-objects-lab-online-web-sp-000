9bc5d9745ccd913bf709f2cf6fbc8a3127897ee3
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-02 21:29:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,17 @@
 class Song
+  attr_accessor :name, :artist
   @@all = []
 
   def initialize(name)
     @name = name
     @@all << self
   end
+
+  def self.all?
+    @@all
+  end
+
+  def self.new_by_filename(filename)
+
 
 end
