a4d41e5d98ba6cb898c51ae69693baf4e59458ea
Silas Michael Knight <silasfelinus@gmail.com>
2020-12-02 20:47:40 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,9 @@
 class Artist
+  @@all = []
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
   def name
@@ -10,6 +12,14 @@ class Artist
 
   def name=(name)
     @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    @songs
   end
 
 end
